e7b4603c379ac32a60dab445be7cf81f2cb4293e
Daniel Scheuermann <scheuerdan@gmx.de>
2025-03-25 14:01:06 +0100

    first commit

--- src/main/java/com/brh/einkaufsplaner_desktop/App.java
+++ src/main/java/com/brh/einkaufsplaner_desktop/App.java
@@ -7,10 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+// homescreen
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("shoppinglist.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("shoppinglist.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 711, 588);
         stage.setTitle("Hello!");
         stage.setScene(scene);
